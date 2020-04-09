b02a9bcee2009cfd7e5fd92d19f2e0d9c29812e1
fenil_23 <fenilshah.osl@gmail.com>
2020-04-09 17:13:48 +0530

    JSX for sidebar Javascript and Genric Componenets Initiating

--- src/index.js
+++ src/index.js
@@ -4,9 +4,28 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as serviceWorker from './serviceWorker';
+import {Helmet} from "react-helmet";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Helmet>
+                {/* Bootstrap core JavaScript*/}
+        <script src="./vendor/jquery/jquery.min.js"></script>
+        <script src="./vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        
+        {/* Core plugin JavaScript*/}
+        <script src="./vendor/jquery-easing/jquery.easing.min.js"></script>
+
+        {/* Custom scripts for all pages*/}
+        <script src="./js/sb-admin-2.min.js"></script>
+
+        {/* Page level plugins */}
+        <script src="./vendor/chart.js/Chart.min.js"></script>
+
+        {/* Page level custom scripts */}
+        <script src="./js/demo/chart-area-demo.js"></script>
+        <script src="./js/demo/chart-pie-demo.js"></script>
+       </Helmet>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
