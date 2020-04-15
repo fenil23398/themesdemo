d44e920fa344b0cd1fefc9bd33b192ee03e0176a
Sneha Singh <sneha19.osl@gmail.com>
2020-04-15 12:32:26 +0530

    GET APIs and database

---
+++ backend/database.js
@@ -0,0 +1,92 @@
+var sqlite3 = require('sqlite3').verbose()
+//verbose modifier is to get extra information for debugging
+
+var md5 = require('md5');
+//MD5 is used to create a hash for stored passwords, avoiding to save plain text passwords.
+
+//Foreign key support - do it in the sqlite3 cli
+// Steps - open cli -> write sqlite3 -> then write the following command 
+// PRAGMA foreign_keys = ON;
+
+//Definition of the SQLite database file (DBSOURCE)
+const DBSOURCE = "db.sqlite"
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+      // Cannot open database
+      console.error(err.message)
+      throw err
+    }else{
+        console.log('Connected to the SQLite database.')
+        db.run(`CREATE TABLE userData (
+            userId INTEGER PRIMARY KEY AUTOINCREMENT,
+            userName text, 
+            createDate TEXT DEFAULT CURRENT_TIMESTAMP,
+            email text UNIQUE, 
+            password text, 
+            CONSTRAINT email_unique UNIQUE (email)
+            )`,
+        (err) => {
+            if (err) {
+                // Table already created
+            }else{
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO userData (userName, email, password) VALUES (?,?,?)'
+                db.run(insert, ["admin","admin@example.com",md5("admin123456")])
+                db.run(insert, ["user","user@example.com",md5("user123456")])
+                db.run(insert, ["abc","abc@example.com",md5("abc123456")])
+                db.run(insert, ["xyz","xyz@example.com",md5("xyz123456")])
+                db.run(insert, ["sneha","sneha@example.com",md5("sneha123456")])
+            }
+        });  
+        db.run(`CREATE TABLE revenueData (
+            revenueId INTEGER PRIMARY KEY AUTOINCREMENT,
+            revenueType text
+            )`,
+        (err) => {
+            if (err) {
+                // Table already created
+            }else{
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO revenueData (revenueType) VALUES (?)'
+                db.run(insert, ["Direct"])
+                db.run(insert, ["Social"])
+                db.run(insert, ["Referral"])
+            }
+        });
+        db.run(`CREATE TABLE earnings (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            timestamp TEXT DEFAULT CURRENT_TIMESTAMP,
+            earningAmount INTEGER,
+            userId INTEGER,
+            revenueId INTEGER,
+            CONSTRAINT fk_userData
+            FOREIGN KEY (userId)
+            REFERENCES userData(userId)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE,
+            CONSTRAINT fk_revenueData
+            FOREIGN KEY (revenueId)
+            REFERENCES revenueData(revenueId)
+            ON UPDATE CASCADE
+            ON DELETE CASCADE
+        )`,
+        (err) => {
+            if(err) {
+                //Table already created
+            }
+            else{
+                //Table just created, creating some rows
+                var insert = 'INSERT INTO earnings (earningAmount, userId, revenueId) VALUES (?,?,?)'
+                db.run(insert, ["100",'1','1'])
+                db.run(insert, ["200",'1','2'])
+                db.run(insert, ["300",'1','3'])
+                db.run(insert, ["400",'4','2'])
+                db.run(insert, ["500",'5','5'])
+            }
+        });
+    }
+});
+
+
+module.exports = db
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,182 @@
-// imports
+//Create express app
+var express = require('express');
+var app = express();
 
-// var express = require('express');
-// var app = express();
-// var port = 5000;
-// var sqlite3 = require('sqlite3');
-// var db = new sqlite3.Database('earningsDb.db');
+//Server port
+var port = 5000;
+
+var db = require("./database.js")
+var md5 = require("md5")
+
+var bodyParser = require('body-parser');
+
+//mounts BodyParser as middleware - every request passes through it
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+// Start server
+app.listen(port, function(){
+    console.log('App started on port '+port);
+});
+
+//get a list of users
+app.get("/users", (req, res, next) => {
+    var sql = "select * from userData"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            // "message":"success",
+            "data":rows
+        })
+      });
+});
+
+//api to check revenue data
+app.get("/revenues", (req, res, next) => {
+    var sql = "select * from revenueData"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            // "message":"success",
+            "data":rows
+        })
+      });
+});
+
+//API to check earnings table data
+app.get("/earnings", (req, res, next) => {
+    var sql = "select * from earnings"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            // "message":"success",
+            "data":rows
+        })
+      });
+});
+
+
+//get a single user by id
+app.get("/users/:userId", (req, res, next) => {
+    var sql = "select * from userData where userId = ?"
+    var params = [req.params.userId]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            // "message":"success",
+            "data":row
+        })
+      });
+});
+
+//Creating a new user
+app.post("/postFormData/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        userName: req.body.userName,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO userData (userName, email, password) VALUES (?,?,?)'
+    var params =[data.userName, data.email, data.password]
+    db.run(sql, params, function (err, result) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            // "message": "success",
+            "data": data,
+            "userId" : this.lastID
+        })
+    });
+})
+
+//POST API for add income form - INCOMPLETE
+app.post('/addIncomeForm/', (req, res, next) => {
+    var errors = []
+    if(!req.body.userId){
+        errors.push("No User Id specified");
+    }
+    if(!req.body.revenueId){
+        errors.push("Revenue Id not specified");
+    }
+    if(!req.body.earningAmount){
+        errors.push("Earning Amount not specified");
+    }
+    if(errors.length){
+        res.status(400).json({"errors":error.join(",")});
+    }
+    var data = {
+        userId : req.body.userId,
+        revenueId : req.body.revenueId,
+        earningAmount : req.body.earningAmount
+    }
+    var sql = "INSERT INTO earnings (earningAmount) SELECT earningAmount FROM earnings JOIN userData ON earnings.userId = userData.userId"
+    // var params = [data.userId, data.earningAmount, data.revenueId]
+    db.run(sql, function(err, result) {
+        if(err) {
+            res.status(400).json({"error":err.message})
+            return;
+        }
+        res.json({
+            "data":data
+        })
+    });
+})
+
+//GET API to return monthwise earnings
+app.get("/getMonthwiseEarnings/:userId", (req, res, next) => {
+    var sql = "SELECT strftime('%m', earnings.timestamp) as month, SUM(earnings.earningAmount) as earningSum FROM earnings CROSS JOIN userData ON earnings.userId = userData.userId WHERE earnings.userId = ? GROUP BY strftime('%m', earnings.timestamp)"
+    var params = [req.params.userId]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            // "message":"success",
+            "data":row
+        })
+      });
+})
+
+//GET API to get revenue type
+app.get('/getRevenueData/:revenueId', (req, res, next) => {
+    var sql = "SELECT revenueType FROM revenueData CROSS JOIN earnings ON earnings.revenueId = revenueData.revenueId WHERE earnings.revenueId = ?"
+    var params = [req.params.revenueId]
+    db.get(sql, params, (err, row) => {
+        if(err) {
+            res.status(400).json({"error":err.message});
+            return;
+        }
+        res.json({
+            "data":row
+        })
+    });
+})
